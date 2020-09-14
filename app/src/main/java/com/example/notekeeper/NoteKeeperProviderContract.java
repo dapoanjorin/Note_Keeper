4edb974eff13e9fa867862e9f3ce21025a40439d
adedapo <femidpo3@gmail.com>
2020-09-14 15:47:00 +0100

    added a contract class for content provider

---
+++ app/src/main/java/com/example/notekeeper/NoteKeeperProviderContract.java
@@ -0,0 +1,35 @@
+package com.example.notekeeper;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public final class NoteKeeperProviderContract {
+
+    private NoteKeeperProviderContract() {
+    }
+
+    public static final String AUTHORITY = "com.example.notekeeper.provider";
+    public static final Uri AUTHORITY_URI = Uri.parse("content://" + AUTHORITY);
+
+    protected interface CoursesIdColumns {
+        String COLUMN_COURSE_ID = "course_id";
+    }
+
+    protected interface CoursesColumns {
+        String COLUMN_COURSE_TITLE = "course_title";
+    }
+
+    protected  interface  NotesColumns {
+        String COLUMN_NOTE_TITLE = "note_title";
+        String COLUMN_NOTE_TEXT = "note_text";
+    }
+    public static final class Courses implements BaseColumns, CoursesColumns, CoursesIdColumns{
+        public static final String PATH = "courses";
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
+    }
+    public static final class Notes implements BaseColumns, NotesColumns, CoursesColumns{
+        public static final String PATH = "notes";
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
+    }
+
+}
